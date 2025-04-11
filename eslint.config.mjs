41e4918c5f30c8b32d231c41339b5d973dafc9e6
DiioBrando <olsaf21f12@gmail.com>
2025-04-11 15:48:41 +0500

    feat: first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import nextPlugin from 'eslint-config-next';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  ...nextPlugin,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules', '.next', 'out', 'build'],
+  },
 ];
-
-export default eslintConfig;
