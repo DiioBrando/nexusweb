a172281cb4fc843d59559620935e218fd81a0e4f
DiioBrando <olsaf21f12@gmail.com>
2025-04-13 15:27:13 +0500

    feat(fix and add): add props classNameTXT, fix heigth

--- src/components/CustomMarquee.tsx
+++ src/components/CustomMarquee.tsx
@@ -4,22 +4,22 @@ import {CircleIcon} from "../../public/svg/CircleIcon";
 
 type ICustomMarquee = ComponentPropsWithoutRef<typeof Marquee>;
 
-export const CustomMarquee = ({ children, ...rest }: ICustomMarquee) => {
+export const CustomMarquee = ({ children, classNameTXT, ...rest }: ICustomMarquee) => {
     return (
         <Marquee {...rest}>
             {children}
             <div className={'flex gap-5 items-center'}>
-                <div className={'text-3xl'}>CREATIVE DESIGN</div>
+                <div className={'text-3xl ' + classNameTXT}>CREATIVE DESIGN</div>
                 <CircleIcon fill={'blue'}/>
-                <div className={'text-3xl'}>UI/UX</div>
+                <div className={'text-3xl ' + classNameTXT}>UI/UX</div>
                 <CircleIcon fill={'white'}/>
-                <div className={'text-3xl'}>MARKETING</div>
+                <div className={'text-3xl ' + classNameTXT}>MARKETING</div>
                 <CircleIcon fill={'green'}/>
-                <div className={'text-3xl'}>MOTION</div>
+                <div className={'text-3xl ' + classNameTXT}>MOTION</div>
                 <CircleIcon fill={'purple'}/>
-                <div className={'text-3xl'}>ANIMATION</div>
+                <div className={'text-3xl ' + classNameTXT}>ANIMATION</div>
                 <CircleIcon fill={'orange'}/>
-                <div className={'text-3xl'}>BRNDING</div>
+                <div className={'text-3xl ' + classNameTXT}>BRNDING</div>
             </div>
         </Marquee>
     );
--- src/features/home-page/HomePage.tsx
+++ src/features/home-page/HomePage.tsx
@@ -1,7 +1,14 @@
+import { CustomMarquee } from "@/components/CustomMarquee";
+
 export const HomePage = () => {
-    return(
-        <div>
-            
+    return (
+        <div className={'h-full w-full relative overflow-hidden'}>
+            <div className={'absolute bottom-0 -rotate-[12.95deg] origin-bottom-left z-20'}>
+                <CustomMarquee className={'bg-neutral-900 h-[60px]'}/>
+            </div>
+            <div className={'absolute bottom-0 right-0 rotate-[50.71deg] origin-bottom-right z-10'}>
+                <CustomMarquee classNameTXT={'text-black'} className={'bg-white h-[60px]'}/>
+            </div>
         </div>
-    )
-}
+    );
+};
