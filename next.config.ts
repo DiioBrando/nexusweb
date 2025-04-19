eaaa34ca2bed30511b81ce85999d63f5ff2ebeeb
DiioBrando <olsaf21f12@gmail.com>
2025-04-19 16:56:35 +0500

    feat(config):  configured host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "5001",
+                pathname: "/uploads/**",
+            },
+        ],
+    },
+    env: {
+        DATABASE_URL: process.env.API_DATABASE_URL,
+    },
 };
 
 export default nextConfig;
