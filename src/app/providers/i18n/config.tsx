ba84030460fd58aa994e948f643058cb4f56b39a
DiioBrando <olsaf21f12@gmail.com>
2025-04-29 02:24:07 +0500

    feat(fix): fix connect i18next

--- src/app/providers/i18n/config.tsx
+++ src/app/providers/i18n/config.tsx
@@ -2,10 +2,29 @@ import i18next from 'i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import {initReactI18next} from 'react-i18next';
+import en from '../../../../public/locales/en/translation.json'
+import ru from '../../../../public/locales/ru/translation.json'
 
-export default i18next.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+i18next.use(Backend).use(LanguageDetector).use(initReactI18next).init({
     fallbackLng: 'en',
+    lng: 'en',
+    debug: true,
+    defaultNS: 'translation',
+    supportedLngs: ['en', 'ru'],
+    resources: {
+        en: {
+            translation: en,
+        },
+        ru: {
+            translation: ru,
+        }
+    },
     interpolation: {
         escapeValue: false,
-    }
+    },
+    backend: {
+        loadPath: "/locales/{{lng}}/translation.json",
+    },
 });
+
+export default i18next;
--- src/app/providers/i18n/i18nProvider.tsx
+++ src/app/providers/i18n/i18nProvider.tsx
@@ -1,6 +1,6 @@
 import {I18nextProvider} from 'react-i18next';
 import {ReactNode} from 'react';
-import i18next from 'i18next';
+import i18next from './config';
 
 export function I18nProvider({ children }: ReactNode) {
     return <I18nextProvider i18n={i18next} defaultNS={'translation'}>{children}</I18nextProvider>
