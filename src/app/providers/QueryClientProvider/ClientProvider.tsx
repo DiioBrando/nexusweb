f3dc0780271b4659050e5b29c049d41c8c0b5e74
DiioBrando <olsaf21f12@gmail.com>
2025-04-30 22:12:07 +0500

    feat(fix): fix type error

--- src/app/providers/QueryClientProvider/ClientProvider.tsx
+++ src/app/providers/QueryClientProvider/ClientProvider.tsx
@@ -2,7 +2,7 @@ import {ReactNode, useState} from "react";
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {queryConfig} from "@/app/providers/QueryClientProvider/config";
 
-export function ClientProvider({ children }: ReactNode) {
+export function ClientProvider({ children }: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient({defaultOptions: queryConfig}));
 
     return(
