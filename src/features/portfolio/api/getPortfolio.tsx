298c534d5de8f07bd7589ef0785ce81b0da64749
DiioBrando <olsaf21f12@gmail.com>
2025-04-30 20:49:13 +0500

    feat: move directory, and add hooks directory

--- src/features/portfolio/api/getPortfolio.tsx
+++ src/features/portfolio/api/getPortfolio.tsx
@@ -1,4 +1,4 @@
-import {api} from "@/lib/api";
+import {api} from "@/shared/lib/api";
 import {IPortfolioProps} from "@/features/portfolio/entities/IPortfolioProps";
 
 export const getPortfolio = async () => await api.get<IPortfolioProps[]>('/get/portfolio');
