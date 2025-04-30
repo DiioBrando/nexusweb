d85d2515da570cdcef13af3eb1007e021ceb6c3e
DiioBrando <olsaf21f12@gmail.com>
2025-04-30 22:08:21 +0500

    feat(fix): fix type error

--- src/app/providers/i18n/i18nProvider.tsx
+++ src/app/providers/i18n/i18nProvider.tsx
@@ -2,6 +2,6 @@ import {I18nextProvider} from 'react-i18next';
 import {ReactNode} from 'react';
 import i18next from './config';
 
-export function I18nProvider({ children }: ReactNode) {
+export function I18nProvider({ children }: { children: ReactNode }) {
     return <I18nextProvider i18n={i18next} defaultNS={'translation'}>{children}</I18nextProvider>
 }
