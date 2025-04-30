3e49d5a8b14775b8340bf870752a899c43e031c8
DiioBrando <olsaf21f12@gmail.com>
2025-04-30 20:42:20 +0500

    feat(changed): changed file name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import AppProviders from "@/app/providers";
 import type { Metadata } from "next";
 import "./globals.css";
-import {Header} from "@/widgets/header";
+import {Header} from "@/widgets/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
